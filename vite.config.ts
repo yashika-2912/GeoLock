896887d3df0d6542734e1036db05a6bc44be85cf
yashika-2912 <yashikav292005@gmail.com>
2026-02-24 21:08:33 +0530

    remove svgr plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      svgrOptions: {
-        icon: true,
-        exportType: "named",
-        namedExport: "ReactComponent",
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
